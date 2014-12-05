17ccd7aa3e4fcb8e5c699c709b7b047ae8e2534e
lukeoglesbee <loglesbee@smu.edu>
2014-12-05 02:08:24 -0600

    fixed cell indexing errors in maze

--- Maze.py
+++ Maze.py
@@ -26,12 +26,10 @@ class Maze:
     """
     self.width = width
     self.height = height
-    self.cell = []
-    for x in range(height):
-      cellRow = []
-      for y in range(width):
-        cellRow.append(Cell("_"))
-      self.cell.append(cellRow)
+    self.cell = {}
+    for x in range(width):
+      for y in range(height):
+        self.cell[x,y] = Cell("_")
 
   def buildWall(self, aIndex, bIndex):
     """
@@ -54,7 +52,7 @@ class Maze:
     """
     x = coord[0]
     y = coord[1]
-    self.cell[x][y].content = content
+    self.cell[x,y].content = content
 
   def __str__(self):
     """
@@ -117,7 +115,7 @@ class Maze:
     y = coords[1]
     if not self.isValidIndex(x,y):
       return False
-    c = self.cell[x][y]
+    c = self.cell[x,y]
     walls = []
     if c.north:
       walls.append(((x,y),(x,y-1)))
@@ -163,32 +161,32 @@ class Maze:
     #Bounds check for the border
     if not self.isValidIndex(aIndex[0],aIndex[1]):
       return False
-    if not self.isValidIndex(bIndex[0], bIndex[1]):
+    if not self.isValidIndex(bIndex[0],bIndex[1]):
       return False
     #Find wall to alter
     xDif = aIndex[0] - bIndex[0]
     yDif = aIndex[1] - bIndex[1]
     if xDif == 1 and yDif == 0:
-      self.cell[aIndex[0]][aIndex[1]].west = isBuild
-      self.cell[bIndex[0]][bIndex[1]].east = isBuild
+      self.cell[aIndex[0],aIndex[1]].west = isBuild
+      self.cell[bIndex[0],bIndex[1]].east = isBuild
     elif xDif == -1 and yDif == 0:
-      self.cell[bIndex[0]][bIndex[1]].west = isBuild
-      self.cell[aIndex[0]][aIndex[1]].east = isBuild
+      self.cell[bIndex[0],bIndex[1]].west = isBuild
+      self.cell[aIndex[0],aIndex[1]].east = isBuild
     elif yDif == 1 and xDif == 0:
-      self.cell[aIndex[0]][aIndex[1]].north = isBuild
-      self.cell[bIndex[0]][bIndex[1]].south = isBuild
+      self.cell[aIndex[0],aIndex[1]].north = isBuild
+      self.cell[bIndex[0],bIndex[1]].south = isBuild
     elif yDif == -1 and xDif == 0:
-      self.cell[bIndex[0]][bIndex[1]].north = isBuild
-      self.cell[aIndex[0]][aIndex[1]].south = isBuild
+      self.cell[bIndex[0],bIndex[1]].north = isBuild
+      self.cell[aIndex[0],aIndex[1]].south = isBuild
     else:
       return False
     if True:
-      a = self.cell[aIndex[0]][aIndex[1]]
-      b = self.cell[bIndex[0]][bIndex[1]]
-      print "-- a (%s,%s)--" % (aIndex[0],aIndex[1])
-      print "N: %s\nS: %s\nE: %s\nW: %s\n" % (a.north, a.south, a.east, a.west)
-      print "-- b (%s,%s)--" % (bIndex[0],bIndex[1])
-      print "N: %s\nS: %s\nE: %s\nW: %s\n" % (b.north, b.south, b.east, b.west)
+      a = self.cell[aIndex[0],aIndex[1]]
+      b = self.cell[bIndex[0],bIndex[1]]
+      # print "-- a (%s,%s)--" % (aIndex[0],aIndex[1])
+      # print "N: %s\nS: %s\nE: %s\nW: %s\n" % (a.north, a.south, a.east, a.west)
+      # print "-- b (%s,%s)--" % (bIndex[0],bIndex[1])
+      # print "N: %s\nS: %s\nE: %s\nW: %s\n" % (b.north, b.south, b.east, b.west)
     return True
 
 
